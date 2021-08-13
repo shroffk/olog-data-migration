2af561a569eca1d636a6990ca659436da4512bc0
Kunal Shroff <kunalshroff9@gmail.com>
2021-08-13 09:14:30 -0400

    Add a status reporting function in the migration service.

--- src/main/java/org/phoebus/olog/OlogMigrationService.java
+++ src/main/java/org/phoebus/olog/OlogMigrationService.java
@@ -1,27 +1,55 @@
 package org.phoebus.olog;
 
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.phoebus.olog.entity.Logbook;
+import org.phoebus.olog.entity.Property;
+import org.phoebus.olog.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
+import static org.phoebus.olog.OlogDataMigrationApplication.logger;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
 public class OlogMigrationService {
 
     @Autowired
     SequenceGenerator generator;
+
+    @Value("${elasticsearch.tag.index:olog_tags}")
+    private String ES_TAG_INDEX;
+    @Value("${elasticsearch.tag.type:olog_tag}")
+    private String ES_TAG_TYPE;
+
+    @Autowired
+    @Qualifier("indexClient")
+    RestHighLevelClient client;
     
-    public void hello() {
-       System.out.println("Hello from Hello Service");
+    public void status() {
+        logger.info("Starting the olog migration process with " + this);
+        try
+        {
+            logger.info("elastic client created: " + client.info(RequestOptions.DEFAULT).toString());
+        } catch (IOException e)
+        {
+            logger.log(Level.SEVERE, "Failed to create elastic client ", e);
+        }
     }
 
-    public void transferTags() {
+    public void transferTags(List<Tag> list) {
         
     }
     
 
-    public void transferLogbooks() {
+    public void transferLogbooks(List<Logbook> logbooks) {
         
     }
     
 
-    public void transferProperties() {
+    public void transferProperties(List<Property> properties) {
         
     }
     
